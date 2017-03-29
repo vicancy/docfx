c951411b6206a51e82bd3ec52e1dd3b0a961aef3
lianwei <lianwei@microsoft.com>
2017-03-29 14:18:15 +0800

    Dashboard page

---
+++ test/Microsoft.DocAsCode.Metadata.ManagedReference.Tests/bin/Debug/net46/cwcoohni.wmt/Example.cs
@@ -0,0 +1,15 @@
+
+using System;
+
+namespace Example
+{
+#region Example
+    static class Program
+    {
+        public int Main(string[] args)
+        {
+            Console.HelloWorld();
+        }
+    }
+#endregion
+}
